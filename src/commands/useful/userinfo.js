cfdfe2947beffece9083193bd1b678041033ca19
MrBoogyBam <MrBoogyBamBusiness@gmail.com>
2020-08-13 10:32:29 +0300

    fixed stuff

--- src/commands/useful/userinfo.js
+++ src/commands/useful/userinfo.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client;
-const config = require("./config.json");
+const config = require('../../../config.json');
 const prefix = config.prefix;
 
 async function userinfoCmd(message) {
